f18178159d28b7a0e7016369663ad87e0ba27085
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-07-23 19:38:25 -0300

    feat: started project

---
+++ lib/app/modules/home/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'home_repository.dart';
+
+class HomeController {
+  final HomeRepository repository;
+
+  HomeController(this.repository);
+}
